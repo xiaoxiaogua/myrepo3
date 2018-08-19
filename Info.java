fd6cdec4fb86cacf955628dbf620ff7357103c18
VictorZhang <zhangshuai_work@outlook.com>
2018-08-19 19:56:08 +0800

    Add Info.java

---
+++ Info.java
@@ -0,0 +1,7 @@
+public class Info {
+  public static void main(String[] args) {
+    System.out.println("**********");
+    System.out.println("***Info***");
+    System.out.println("**********");
+  }
+}
